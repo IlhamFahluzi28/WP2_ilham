3fdf5afa146c38c490b8164790a9b4222eaaa8c9
IlhamFahluzi28 <ilhamfahluzi413@gmail.com>
2023-10-24 16:42:42 +0700

    push pustaka booking

---
+++ application/views/Pustaka booking/Web .php
@@ -0,0 +1,55 @@
+<?php
+class Latihan1 extends C1_Controller
+{
+    public function index{}
+[
+    echo"Selamat Datang..selamat belajar Web Programming";
+
+}
+
+}
+
+<?php
+class Model_latihan1 extends C1_Model
+{
+    //membuat Variable untuk menampung nilai
+    public $nilail,$nilai2, $hasil;
+    //method penjumlahan
+    public function Jumlah($n1 = null,$n2 = null)
+    {
+       $this->nilai1 = $nilI;
+       $this->nilai2 = $nil2;
+       $this->hasil = $this->nilai1 + $this ->nilai2;
+       return $this-hasil;
+
+    }
+    
+]<?PHP
+class Latihan1 extendS C1_controller
+[
+    public funtion index()
+    [
+        echo"Selamat Datang.. selamat belajar Web Programing";
+        //$this->load->VieW('View-latihan1');
+    }
+
+       $this->load->model('Model_latihan1');
+       $hasil = $this->Model_latihan->jumlah($n1, $n2);
+       echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." ="
+.$hasil;
+   ]
+}
+<html>
+
+
+<head>
+<title>Latihan 1</title>
+</head>
+
+<body
+    
+    ]
+
+    )
+]
+    
